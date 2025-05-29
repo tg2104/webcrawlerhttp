6c5c69e9831149558faa3c4d6d8fe52d2ea651ed
Tishya Ghosh <tishyaghosh@Tishyas-MacBook-Air.local>
2025-05-29 23:20:11 +0530

    application basics

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { crawlPage } = require('./crawl.js')
-function main () {
+async function main () {
    if(process.argv.length < 3){
         console.log("no website provided")
         process.exit(1)
@@ -10,8 +10,9 @@ function main () {
    }
    const baseURL = process.argv[2]
    console.log(`starting crawling of ${baseURL}`)
-   crawlPage(baseURL)
+   const pages = await crawlPage(baseURL, baseURL, {})
+   for (const page in Object.entries(pages)){
+        console.log(page)
+   }
 }
-
-
 main()
